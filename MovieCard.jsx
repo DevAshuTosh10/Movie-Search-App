d15e9bbbd8526aa8a5aa958b3275ce796aae0fda
ASHUTOSH SATPATHY <147174405+DevAshuTosh10@users.noreply.github.com>
2025-02-17 19:55:16 +0530

    Update MovieCard.jsx

--- MovieCard.jsx
+++ MovieCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
   return (
     <div className="movie" key={imdbID}>
-      <div>
+      <div></div>
         <p>{Year}</p>
       </div>
 
